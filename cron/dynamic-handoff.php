128f0f4a1038438a52aac9fc3bcedae655fb3323
Ankur <ankur.vyas@marutitech.com>
2022-02-15 16:01:25 +0530

    - Update query logic for re-assign agent dynamic-handoff

--- cron/dynamic-handoff.php
+++ cron/dynamic-handoff.php
@@ -55,7 +55,36 @@ function sb_init() {
 }
 
 function sb_get_unassigned_conversations($minute, $count, $department) {
-    $conversations = sb_db_get('SELECT sb_conversations.id, sb_conversations.user_id, sb_conversations.agent_id, sb_conversations.dynamic_handoff_count FROM sb_conversations INNER JOIN sb_messages ON sb_conversations.id = sb_messages.conversation_id WHERE sb_conversations.status_code IN(0, 1, 2, 6) AND sb_conversations.source = "sm" AND sb_conversations.dynamic_handoff_count < ' . $count . ' AND sb_conversations.department = ' . $department . ' AND sb_messages.id IN (SELECT MAX(sb_messages.id) FROM sb_messages inner join sb_users ON sb_messages.user_id = sb_users.id WHERE sb_messages.creation_time < (UTC_TIMESTAMP() - INTERVAL '. $minute .' MINUTE) AND sb_users.user_type not in ("admin", "agent") GROUP BY sb_messages.conversation_id) ORDER BY sb_conversations.id DESC', false);
+    // $conversations = sb_db_get('SELECT sb_conversations.id, sb_conversations.user_id, sb_conversations.agent_id, sb_conversations.dynamic_handoff_count FROM sb_conversations INNER JOIN sb_messages ON sb_conversations.id = sb_messages.conversation_id WHERE sb_conversations.status_code IN(0, 1, 2, 6) AND sb_conversations.source = "sm" AND sb_conversations.dynamic_handoff_count < ' . $count . ' AND sb_conversations.department = ' . $department . ' AND sb_messages.id IN (SELECT MAX(sb_messages.id) FROM sb_messages inner join sb_users ON sb_messages.user_id = sb_users.id WHERE sb_messages.creation_time < (UTC_TIMESTAMP() - INTERVAL '. $minute .' MINUTE) AND sb_users.user_type not in ("admin", "agent") GROUP BY sb_messages.conversation_id) ORDER BY sb_conversations.id DESC', false);
+    
+    // conv with last message before 60 minutes
+    $conversations = sb_db_get('SELECT sb_messages.conversation_id FROM sb_messages WHERE sb_messages.id IN(SELECT MAX(sb_messages.id) FROM sb_messages INNER JOIN sb_conversations ON sb_messages.conversation_id = sb_conversations.id WHERE sb_conversations.status_code IN (0 , 1, 2, 6) AND sb_conversations.source = "sm" AND sb_conversations.dynamic_handoff_count < ' . $count . ' AND sb_conversations.department = ' . $department . ' GROUP BY sb_messages.conversation_id) AND sb_messages.creation_time < (UTC_TIMESTAMP() - INTERVAL ' . $minute . ' MINUTE)', false);
+    $time_conversation_list = array();
+    $time_conversation_comma_str = null;
+    if ($conversations) {
+        foreach ($conversations as $key => $value) {
+            array_push($time_conversation_list, $value['conversation_id']);
+        }
+        $time_conversation_comma_str = implode(",", $time_conversation_list);
+    }
+
+    // conv list where admin agent have sent atleast one message
+    $conversation_list_comma_str = null;
+    if ($time_conversation_comma_str) {
+        $conversations = sb_db_get('SELECT sb_messages.conversation_id FROM sb_messages INNER JOIN sb_users ON sb_messages.user_id = sb_users.id INNER JOIN sb_conversations ON sb_messages.conversation_id = sb_conversations.id WHERE sb_users.user_type IN("admin" , "agent") AND sb_conversations.status_code IN (0 , 1, 2, 6) AND sb_conversations.source = "sm" AND sb_conversations.dynamic_handoff_count < ' . $count . ' AND sb_conversations.department = ' . $department . ' AND sb_conversations.id IN ('. $time_conversation_comma_str .') AND sb_messages.user_id <> 1 AND sb_messages.message <> "" GROUP BY sb_messages.conversation_id', false);
+        $agent_admin_conversation_list = array();
+        foreach ($conversations as $key => $value) {
+            array_push($agent_admin_conversation_list, $value['conversation_id']);
+        }
+        $conversation_list = array_diff($time_conversation_list, $agent_admin_conversation_list);
+        $conversation_list_comma_str = implode(",", $conversation_list);
+    }
+
+    // conv list which are older then 60 minutes and does not contain the message sent by admin or agent
+    $conversations = array();
+    if ($conversation_list_comma_str) {
+        $conversations = sb_db_get('SELECT sb_conversations.id, sb_conversations.user_id, sb_conversations.agent_id, sb_conversations.dynamic_handoff_count FROM sb_conversations WHERE sb_conversations.status_code IN(0, 1, 2, 6) AND sb_conversations.source = "sm" AND sb_conversations.dynamic_handoff_count < ' . $count . ' AND sb_conversations.department = ' . $department . '  AND sb_conversations.id IN ( '. $conversation_list_comma_str .' ) ORDER BY sb_conversations.id DESC', false);
+    }
     return $conversations;
 }
 
